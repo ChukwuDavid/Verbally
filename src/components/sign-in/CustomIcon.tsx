1f8fc4dd34ae2a31a501ac408ecb9288b164344d
David C. John <david.cjohn@outlook.com>
2025-01-06 01:06:10 +0100

    Sign-up and sign-in feature addition to the app build

---
+++ src/components/sign-in/CustomIcon.tsx
@@ -0,0 +1,52 @@
+
+import * as React from 'react';
+
+import SvgIcon from '@mui/material/SvgIcon';
+
+
+
+export function GoogleIcon(props: any) {
+
+  return (
+
+    <SvgIcon {...props}>
+
+      {/* SVG path for Google icon */}
+
+    </SvgIcon>
+
+  );
+
+}
+
+
+
+export function FacebookIcon(props: any) {
+
+  return (
+
+    <SvgIcon {...props}>
+
+      {/* SVG path for Facebook icon */}
+
+    </SvgIcon>
+
+  );
+
+}
+
+
+
+export function SitemarkIcon(props: any) {
+
+  return (
+
+    <SvgIcon {...props}>
+
+      {/* SVG path for Sitemark icon */}
+
+    </SvgIcon>
+
+  );
+
+}
